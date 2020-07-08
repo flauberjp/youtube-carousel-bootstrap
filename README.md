9ca9264861d87f47008be34dd0622f3820a73e2a
Flaviano Flauber <flauberjp@gmail.com>
2020-07-08 00:15:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Reference:
+
+- [Responsive Image & Youtube Video Carousel For Bootstrap 4](https://www.jqueryscript.net/slider/youtube-carousel-bootstrap.html)
+- [Bootstrap Carousel Plugin](https://www.w3schools.com/bootstrap/bootstrap_carousel.asp)
+- [Bootstrap JS Carousel](https://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp)
+- [YouTube Player API Reference for iframe Embeds](https://developers.google.com/youtube/iframe_api_reference#Queueing_Functions)
+- [Parâmetros do player incorporado do YouTube](https://developers.google.com/youtube/player_parameters.html?playerVersion=HTML5&hl=pt-br)
+- [Carousel](https://getbootstrap.com/docs/4.0/components/carousel/)
---
+++ index.js
@@ -0,0 +1,103 @@
+var videoList = [];
+var captionsList = [];
+var videoRef;
+
+window.onload = (event) => {
+  var i;
+  var carouselItems = $(".video-player");
+  for (i = 0; i < carouselItems.length; i++) {
+    var video = carouselItems[i];
+    videoList.push(video);
+    captionsList.push(video.getAttribute("caption"));
+  }
+  videoRef = videoList[0];
+  createVideo(videoRef);
+  document
+    .getElementsByClassName("carousel-control-prev-icon")[0]
+    .addEventListener("click", function () {
+      mudarVideo("-");
+    });
+
+  document
+    .getElementsByClassName("carousel-control-next-icon")[0]
+    .addEventListener("click", function () {
+      mudarVideo("+");
+    });
+};
+
+function mudarVideo(action) {
+  var index = videoList.indexOf(videoRef);
+  if (action === "+") {
+    index += 1;
+    if (index > videoList.length - 1) {
+      index = 0;
+    }
+  } else {
+    index -= 1;
+    if (index < 0) {
+      index = videoList.length - 1;
+    }
+  }
+  videoRef = videoList[index];
+  console.debug(
+    "Posição no carrosel: " +
+      index +
+      "; Youtube video id: " +
+      videoRef.getAttribute("data-video-id")
+  );
+  player.loadVideoById({
+    videoId: videoRef.getAttribute("data-video-id"),
+  });
+
+  atualizarCaption();
+}
+
+function createVideo(video) {
+  var youtubeScriptId = "youtube-api";
+  var youtubeScript = document.getElementById(youtubeScriptId);
+
+  var tag = document.createElement("script");
+  var firstScript = document.getElementsByTagName("script")[0];
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  tag.id = youtubeScriptId;
+  firstScript.parentNode.insertBefore(tag, firstScript);
+
+  var videoId = video.getAttribute("data-video-id");
+
+  atualizarCaption();
+
+  window.onYouTubeIframeAPIReady = function () {
+    window.player = new window.YT.Player(video, {
+      videoId: videoId,
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+      playerVars: {
+        autoplay: 1,
+        modestbranding: 1,
+        rel: 0,
+        controls: 0,
+      },
+    });
+  };
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === 0) {
+    mudarVideo("+");
+  }
+}
+
+function atualizarCaption() {
+  var caption = videoRef.getAttribute("caption");
+  console.debug("Valor do caption: " + caption);
+
+  var carouselCaption = $(".carousel-caption")[0];
+  carouselCaption.textContent = caption;
+}
